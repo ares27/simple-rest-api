33fb84514d58793bd7fd8a9c3ef9261e248f6404
Byron Jehoma <byron.jehoma@dfafrica.co.za>
2021-07-15 09:39:11 +0200

    updated.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const PORT = process.env.PORT || 3099;
 const express = require("express");
 const app = express();
-const api = require("./routes/api");
-const countriesapi = require("./routes/countries-api");
-const activitiesapi = require("./routes/activities-api");
+const api = require("/routes/api");
+const activitiesapi = require("/routes/activities-api");
+// const countriesapi = require("./routes/countries-api");
 
 app.use(express.json());
 app.use("/api", api);
 app.use("/api", activitiesapi);
-app.use("/api", countriesapi);
+// app.use("/api", countriesapi);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
